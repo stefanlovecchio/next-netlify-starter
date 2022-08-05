55436f5b6435a75eb7ec0f25315144d1a70020d0
stefanlovecchio <99218647+stefanlovecchio@users.noreply.github.com>
2022-08-04 22:03:33 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '@styles/globals.css'
+/**
+ * WEB222 – Assignment 04
+ *
+ * I declare that this assignment is my own work in accordance with
+ * Seneca Academic Policy. No part of this assignment has been
+ * copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ * Please update the following with your information:
+ *
+ *      Name:       Stefan Lovecchio
+ *      Student ID: 154725212
+ *      Date:       Aug 04, 2022
+ */
 
-function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const box = document.getElementById("box");
+//when the page loads, hide the box
+box.style.display = "none";
+
+function handleClick() {
+    if (document.getElementById("hiring").checked) {
+        box.style.display = "block";
+    } else {
+        box.style.display = "none";
+    }
 }
 
-export default Application
+const radioBtn = document.querySelectorAll("input[type=radio]");
+radioBtn.forEach((radio) => {
+    radio.addEventListener("click", handleClick);
+});
